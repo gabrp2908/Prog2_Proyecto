4ffc5def5946fa61899b51decc1d16d99cbe056a
Gabriela <gabrp2908@gmail.com>
2024-11-10 23:51:41 -0400

    feat: Se agregaron las estructuras de datos en el codigo

--- src/codigo.cpp
+++ src/codigo.cpp
@@ -2,3 +2,22 @@
 #include <fstream>
 
 using namespace std;
+
+struct Vehiculos{
+    string modelo, marca, placa, color, motor, fecha_de_entrega;
+    int year, kilometraje, ced_cliente;
+    float precio_renta;
+    bool rentado;
+};
+
+struct Cliente{
+    string nombre, apellido, email, direccion;
+    int cedula, cantidad_vehiculos_rentados;
+    bool activo;
+};
+
+struct Repuestos{
+    string modelo, marca, nombre, modelo_carro;
+    int year_carro, existencias;
+    float precio;
+};
