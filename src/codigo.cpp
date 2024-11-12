1362b606dee20b2ebacfddead530d627cdbcc329
erickMDR <erickdiazr27@gmail.com>
2024-11-11 22:41:52 -0400

    feat: Se agregó la función de lectura para los datos de vehiculos

--- src/codigo.cpp
+++ src/codigo.cpp
@@ -25,6 +25,7 @@ struct Repuestos{
 }repuesto;
 
 void Lectura_Cliente();
+void Lectura_Vehiculo();
 
 void Lectura_Cliente() {
 
@@ -49,6 +50,38 @@ void Lectura_Cliente() {
     }
 
     lectura_cliente.close();
+}
+
+void Lectura_Vehiculo() {
+
+    ifstream lectura_vehiculo("DATOS_VEHICULOS.csv", ios::in);
+    string line;
+
+    getline(lectura_vehiculo, line);
+
+    while(getline(lectura_vehiculo, line)) {
+
+        istringstream ss(line);
+
+        getline(ss, vehiculo.modelo, ',');
+        getline(ss, vehiculo.marca, ',');
+        getline(ss, vehiculo.placa, ',');
+        getline(ss, vehiculo.color, ',');
+        ss >> vehiculo.year;
+        ss.ignore();
+        ss >> vehiculo.kilometraje;
+        ss.ignore();
+        ss >> vehiculo.rentado;
+        ss.ignore();
+        getline(ss, vehiculo.motor, ',');
+        ss >> vehiculo.precio_renta;
+        ss.ignore();
+        ss >> vehiculo.ced_cliente;
+        ss.ignore();
+        getline(ss, vehiculo.fecha_de_entrega);
+    }
+
+    lectura_vehiculo.close();
 }
 
 int main() {
