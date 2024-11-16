453a196846f36cc819166dfc121f2ea6c94c0189
erickMDR <erickdiazr27@gmail.com>
2024-11-16 00:21:02 -0400

    feat: Se añadieron las funciones de consulta

--- src/codigo.cpp
+++ src/codigo.cpp
@@ -354,6 +354,154 @@ void Lectura_Repuesto() {
     QuickSortRepuestos(0, numRepuestos - 1);
 }
 
+void Consulta_Cliente() {
+    int cedula;
+    cout << "Ingrese la cedula del cliente que desea consultar: "; cin >> cedula;
+
+    int left = 0, right = numClientes - 1;
+    bool encontrado = false;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (clientes[mid].cedula == cedula) {
+            encontrado = true;
+            cout << " Cedula: " << clientes[mid].cedula << endl;
+            cout << " Nombre: " << clientes[mid].nombre << endl;
+            cout << " Apellido: " << clientes[mid].apellido << endl;
+            cout << " Correo: " << clientes[mid].email << endl;
+            cout << " Vehiculos rentados: " << clientes[mid].cantidad_vehiculos_rentados << endl;
+            cout << " Direccion: " << clientes[mid].direccion << endl;
+            cout << " Activo: " << (clientes[mid].activo ? "Si" : "No") << endl;
+            break;
+        }
+        if (clientes[mid].cedula < cedula) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    if (!encontrado) {
+        cout << "Cedula " << cedula << " no encontrada." << endl;
+    }
+}
+
+void Consulta_Vehiculo_x_placa() {
+    ifstream lectura_vehiculo("DATOS_VEHICULOS.csv");
+	string line, vehiculo_buscar, vehiculo_actual;
+	bool existe = false;
+	cout << "Ingrese la placa del vehiculo que desea consultar: "; cin >> vehiculo_buscar;
+	getline(lectura_vehiculo, line);
+	while (getline(lectura_vehiculo, line)) {
+		istringstream ss(line);
+		
+        getline(ss, vehiculo.modelo, ',');
+        getline(ss, vehiculo.marca, ',');
+        getline(ss, vehiculo.placa, ',');
+        vehiculo_actual = vehiculo.placa;
+        getline(ss, vehiculo.color, ',');
+        ss >> vehiculo.year;
+        ss.ignore();
+        ss >> vehiculo.kilometraje;
+        ss.ignore();
+        ss >> vehiculo.rentado;
+        ss.ignore();
+        getline(ss, vehiculo.motor, ',');
+        ss >> vehiculo.precio_renta;
+        ss.ignore();
+        ss >> vehiculo.ced_cliente;
+        ss.ignore();
+        getline(ss, vehiculo.fecha_de_entrega);
+
+		if (vehiculo_actual.compare(vehiculo_buscar) == 0) {
+			existe = true;
+			cout << " Modelo:" << vehiculo.modelo << endl;
+			cout << " Marca: " << vehiculo.marca << endl;
+			cout << " Placa: " << vehiculo.placa << endl;
+			cout << " Color: " << vehiculo.color << endl;
+            cout << " Year: " << vehiculo.year << endl;
+            cout << " Kilometraje: " << vehiculo.kilometraje << " Km" << endl;
+            cout << " Rentado: " << (vehiculo.rentado? "Si" : "No")<< endl;
+            cout << " Motor: " << vehiculo.motor << endl;
+            cout << " Precio de renta: " << vehiculo.precio_renta << " $/hr" << endl;
+            cout << " Cedula del cliente: " << vehiculo.ced_cliente << endl;
+            cout << " Fecha de entrega: " << vehiculo.fecha_de_entrega << endl;
+		}
+	}
+	lectura_vehiculo.close();
+	if (!existe) {
+		cout << "Placa " << vehiculo_buscar << " no encontrado.";
+	}
+}
+
+void Consulta_Vehiculo_x_cedula() {
+	int cedula;
+	cout << "Ingrese la cedula del cliente que alquilo el carro que desea consultar: "; cin >> cedula;
+
+    int left = 0, right = numVehiculos - 1;
+    bool encontrado = false;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+	    if (vehiculos[mid].ced_cliente == cedula) {
+		    encontrado = true;
+		    cout << " Modelo: " << vehiculos[mid].modelo << endl;
+		    cout << " Marca: " << vehiculos[mid].marca << endl;
+		    cout << " Placa: " << vehiculos[mid].placa << endl;
+		    cout << " Color: " << vehiculos[mid].color << endl;
+            cout << " Year: " << vehiculos[mid].year << endl;
+            cout << " Kilometraje: " << vehiculos[mid].kilometraje << " Km" << endl;
+            cout << " Rentado: " << (vehiculos[mid].rentado? "Si" : "No")<< endl;
+            cout << " Motor: " << vehiculos[mid].motor << endl;
+            cout << " Precio de renta: " << vehiculos[mid].precio_renta << " $/hr" << endl;
+            cout << " Cedula del cliente: " << vehiculos[mid].ced_cliente << endl;
+            cout << " Fecha de entrega: " << vehiculos[mid].fecha_de_entrega << endl;
+            break;
+	    }
+        if (vehiculos[mid].ced_cliente < cedula) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    if (!encontrado) {
+        cout << "Cedula " << cedula << " no encontrada." << endl;
+    }
+}
+
+void Consulta_Repuesto() {
+    int modelo;
+	cout << "Ingrese el modelo del repuesto que desea consultar: "; cin >> modelo;
+
+    int left = 0, right = numVehiculos - 1;
+    bool encontrado = false;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (repuestos[mid].modelo == modelo) {
+		    encontrado = true;
+			cout << " Modelo: " << repuestos[mid].modelo << endl;
+			cout << " Marca: " << repuestos[mid].marca << endl;
+			cout << " Nombre: " << repuestos[mid].nombre << endl;
+			cout << " Modelo del carro: " << repuestos[mid].modelo_carro << endl;
+            cout << " Year Carro: " << repuestos[mid].year_carro << endl;
+            cout << " Precio: " << repuestos[mid].precio << " $" << endl;
+            cout << " Existencias: " << repuestos[mid].existencias << endl;
+            break;
+        }
+        if (repuestos[mid].modelo < modelo) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    if (!encontrado) {
+        cout << "Modelo " << modelo << " no encontrado." << endl;
+    }
+}
+
 int main() {
     Lectura_Cliente();
     Lectura_Vehiculo();
